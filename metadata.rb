f0970085a339d83263e0ebd6a0b04e4fdc4f8b3b
Linus Ruth <linus.ruth@changepoint.com>
2018-04-24 18:13:07 -0700

    chore: update metadata.rb per foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
-name             'cacert'
-maintainer       'Shawn Weitzel'
-maintainer_email 'shawn.weitzel@changepoint.com'
-license          'all_rights'
-description      'Installs/Configures cacert'
-long_description 'Installs/Configures cacert'
-version          '0.1.1'
+name 'cacert'
+maintainer 'Changepoint'
+maintainer_email 'cpc_sea_teamengineering@changepoint.com'
+license 'All rights reserved'
+description 'Installs/Configures cacert'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+ver_path = File.join(File.dirname(__FILE__), 'version.txt')
+version File.exist?(ver_path) ? IO.read(ver_path).chomp : '0.2.1'
+chef_version '>= 12.5' if respond_to?(:chef_version)
+issues_url 'https://github.com/daptiv/daptiv_sso/issues'
+source_url 'https://github.com/daptiv/daptiv_sso/'
+supports 'ubuntu'
+supports 'windows'
