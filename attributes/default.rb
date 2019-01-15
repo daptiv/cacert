8ab6ef1b0364c48c9fc38d4328dca310c645f88e
Linus Ruth <linus.ruth@changepoint.com>
2019-01-15 12:55:37 -0800

    chore: update cacert.pem, source from artifactory

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 #:encoding utf-8
-default['cacert']['pem_url'] = 'http://curl.haxx.se/ca/cacert.pem'
+default['cacert']['pem_url'] =
+  'http://artrepo.daptiv.com/artifactory/simple/cacert-local/' \
+  'cacert-2018-12-05.pem'
 default['cacert']['install_path'] = Chef::Config[:file_cache_path]
