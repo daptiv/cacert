017a6c05d9f61ea3502f4dfd3d11a05bff8d4de9
Linus Ruth <linus.ruth@changepoint.com>
2019-01-31 12:46:48 -0800

    fix: broken test, wrong attribute path

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -3,14 +3,31 @@
 require 'minitest/spec'
 
 # The Install tests
+# rubocop:disable BlockLength
 class CacertInstallTest < MiniTest::Chef::Spec
   describe 'cacert::default' do
     it 'ensure_pem_file_exists' do
-      assert File.exist?("#{node['cacert']['install_path']}/cacert.pem")
+      case node['platform']
+      when 'linux'
+        assert File.exist?(
+          "#{node['cacert']['linux']['install_path']}/cacert.pem"
+        )
+      when 'windows'
+        assert File.exist?(
+          "#{node['cacert']['windows']['install_path']}\\cacert.pem"
+        )
+      end
     end
 
     it 'ensures SSL_CERT_FILE environment variable is in registry' do
       case node['platform']
+      when 'linux'
+        cert_file_path = "#{node['cacert']['linux']['install_path']}/cacert.pem"
+        assert(
+          File.foreach('/etc/environment').grep(
+            /SSL_CERT_FILE=#{cert_file_path}/
+          ).any?
+        )
       when 'windows'
         require 'win32/registry'
         path = 'SYSTEM\\CurrentControlSet\\Control\\' \
@@ -19,7 +36,7 @@ class CacertInstallTest < MiniTest::Chef::Spec
         reg_type = Win32::Registry::KEY_READ | 0x100
         Win32::Registry::HKEY_LOCAL_MACHINE.open(path, reg_type) do |reg|
           assert_equal(
-            "#{node['cacert']['install_path']}\\cacert.pem",
+            "#{node['cacert']['windows']['install_path']}\\cacert.pem",
             reg[key],
             'SSL_CERT_FILE environment variable missing or incorrect'
           )
@@ -28,3 +45,4 @@ class CacertInstallTest < MiniTest::Chef::Spec
     end
   end
 end
+# rubocop:enable BlockLength
